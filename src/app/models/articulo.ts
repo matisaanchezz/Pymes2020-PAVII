6d9585e0d99cb6e7f267398903fe8015dc3228c8
Matias Sanchez <matigonsan4935@gmail.com>
2020-10-02 01:52:13 -0300

    componente articulos

---
+++ src/app/models/articulo.ts
@@ -0,0 +1,113 @@
+export class Articulo {
+  IdArticulo: number;
+  Nombre: string;
+  Precio: number;
+  CodigoDeBarra: string;
+  IdArticuloFamilia: number;
+  Stock: number;
+  FechaAlta: string;
+  Activo: boolean;
+}
+
+export const Articulos: Articulo[] = [
+  {
+    IdArticulo: 108,
+    Nombre: 'Adaptador usb wifi tl-wn722n',
+    Precio: 219.0,
+    CodigoDeBarra: '0693536405046',
+    IdArticuloFamilia: 9,
+    Stock: 898,
+    FechaAlta: '2017-01-23T00:00:00',
+    Activo: false,
+  },
+  {
+    IdArticulo: 139,
+    Nombre: 'Aire acondicionado daewoo 3200fc dwt23200fc',
+    Precio: 5899.0,
+    CodigoDeBarra: '0779816944014',
+    IdArticuloFamilia: 7,
+    Stock: 668,
+    FechaAlta: '2017-01-04T00:00:00',
+    Activo: true,
+  },
+  {
+    IdArticulo: 140,
+    Nombre: 'Aire acondicionado durabrand 3500fc dus35wcl4',
+    Precio: 5499.0,
+    CodigoDeBarra: '0779688543933',
+    IdArticuloFamilia: 7,
+    Stock: 945,
+    FechaAlta: '2017-01-20T00:00:00',
+    Activo: true,
+  },
+  {
+    IdArticulo: 141,
+    Nombre: 'Aire acondicionado durabrand 4500fc dus53wcl4',
+    Precio: 7499.0,
+    CodigoDeBarra: '0779688543937',
+    IdArticuloFamilia: 7,
+    Stock: 962,
+    FechaAlta: '2016-12-29T00:00:00',
+    Activo: true,
+  },
+  {
+    IdArticulo: 142,
+    Nombre: 'Aire acondicionado kelvinator 2500wfc cod1056',
+    Precio: 4499.0,
+    CodigoDeBarra: '0779694101056',
+    IdArticuloFamilia: 7,
+    Stock: 670,
+    FechaAlta: '2017-01-03T00:00:00',
+    Activo: true,
+  },
+  {
+    IdArticulo: 143,
+    Nombre: 'Aire acondicionado lg 3000 fc h126tnw0',
+    Precio: 7499.0,
+    CodigoDeBarra: '0779808338858',
+    IdArticuloFamilia: 7,
+    Stock: 441,
+    FechaAlta: '2017-01-09T00:00:00',
+    Activo: true,
+  },
+  {
+    IdArticulo: 144,
+    Nombre: 'Aire acondicionado lg 4500 fc h1865nw0',
+    Precio: 10399.0,
+    CodigoDeBarra: '0779808338859',
+    IdArticuloFamilia: 7,
+    Stock: 971,
+    FechaAlta: '2016-12-23T00:00:00',
+    Activo: true,
+  },
+  {
+    IdArticulo: 145,
+    Nombre: 'Aire acondicionado lg 5500 fc h2465nw0',
+    Precio: 12699.0,
+    CodigoDeBarra: '0779808338860',
+    IdArticuloFamilia: 7,
+    Stock: 648,
+    FechaAlta: '2017-01-15T00:00:00',
+    Activo: true,
+  },
+  {
+    IdArticulo: 146,
+    Nombre: 'Aire acondicionado lg artcool 2300fc h096eft0',
+    Precio: 7999.0,
+    CodigoDeBarra: '0779808338853',
+    IdArticuloFamilia: 7,
+    Stock: 659,
+    FechaAlta: '2017-01-01T00:00:00',
+    Activo: true,
+  },
+  {
+    IdArticulo: 147,
+    Nombre: 'Aire acondicionado lg artcool 4500fc h1868ft0',
+    Precio: 12899.0,
+    CodigoDeBarra: '0779808338855',
+    IdArticuloFamilia: 7,
+    Stock: 712,
+    FechaAlta: '2016-12-25T00:00:00',
+    Activo: true,
+  },
+];
